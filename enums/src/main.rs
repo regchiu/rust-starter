ec3c7e37150f8c58c20be408c50beac9df457a00
regchiu <regx1385@gmail.com>
2022-12-27 11:27:40 +0800

    feat: enums

---
+++ enums/src/main.rs
@@ -0,0 +1,103 @@
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+enum IpAddr2 {
+    V4(String),
+    V6(String),
+}
+
+enum IpAddr3 {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn main() {
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+
+    route(four);
+    route(six);
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    let home2 = IpAddr2::V4(String::from("127.0.0.1"));
+    let loopback2 = IpAddr2::V6(String::from("::1"));
+
+    let home3 = IpAddr3::V4(127, 0, 0, 1);
+    let loopback3 = IpAddr3::V6(String::from("::1"));
+
+    // Option
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+    // Error!
+    // let sum = x + y;
+
+    let coin = Coin::Penny;
+    println!("{}", value_in_cents(coin));
+
+    // Option<T>
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("five: {:#?}", five);
+    println!("six: {:#?}", six);
+    println!("none: {:#?}", none);
+
+    // if let...
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+}
+
+fn route(ip_kind: IpAddrKind) {}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucy coin!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
